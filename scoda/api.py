8419bdf3f95eea710f50fde6f4abca2807c25f29
tmotshabi <tshiamo@opencitieslab.org>
2023-08-22 09:48:36 +0200

    Revert back to african countries only

--- scoda/api.py
+++ scoda/api.py
@@ -226,22 +226,22 @@ def api_explore(check):
 
     # Filter countries
     countries = []
-    if request.args.get('african') == 'african':
-        # Filter the DataFrame to get data for African countries
-        countries = [
-                'Algeria', 'Angola','Benin','Botswana','Burkina Faso',
-                'Burundi','Cabo Verde','Cameroon','Central African Republic',
-                'Chad','Comoros','Democratic Republic of the Congo',
-                'Republic of the Congo','Cote d\'Ivoire','Djibouti','Egypt',
-                'Equatorial Guinea','Eritrea','Ethiopia','Gabon','Gambia',
-                'Ghana','Guinea','Guinea Bissau','Kenya','Lesotho',
-                'Liberia','Libya','Madagascar','Malawi','Mali','Mauritania',
-                'Mauritius','Morocco','Mozambique','Namibia','Niger','Nigeria',
-                'Rwanda','Sao Tome and Principe','Senegal','Seychelles',
-                'Sierra Leone','Somalia','South Africa','South Sudan','Sudan',
-                'Swaziland','Tanzania','Togo','Tunisia','Uganda','Zambia','Zimbabwe'
-        ]
-        filtered_df = filtered_df[df['re_name'].isin(countries)]
+    # if request.args.get('african') == 'african':
+    # Filter the DataFrame to get data for African countries
+    countries = [
+            'Algeria', 'Angola','Benin','Botswana','Burkina Faso',
+            'Burundi','Cabo Verde','Cameroon','Central African Republic',
+            'Chad','Comoros','Democratic Republic of the Congo',
+            'Republic of the Congo','Cote d\'Ivoire','Djibouti','Egypt',
+            'Equatorial Guinea','Eritrea','Ethiopia','Gabon','Gambia',
+            'Ghana','Guinea','Guinea Bissau','Kenya','Lesotho',
+            'Liberia','Libya','Madagascar','Malawi','Mali','Mauritania',
+            'Mauritius','Morocco','Mozambique','Namibia','Niger','Nigeria',
+            'Rwanda','Sao Tome and Principe','Senegal','Seychelles',
+            'Sierra Leone','Somalia','South Africa','South Sudan','Sudan',
+            'Swaziland','Tanzania','Togo','Tunisia','Uganda','Zambia','Zimbabwe'
+    ]
+    filtered_df = filtered_df[df['re_name'].isin(countries)]
 
 
     if filtered_df.empty:
